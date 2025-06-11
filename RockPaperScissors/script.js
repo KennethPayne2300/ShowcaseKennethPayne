3c77027b1d509820d8b44938427f46cac189736b
KennethPayne2300 <kennethpayne353@gmail.com>
2025-06-11 14:48:04 -0500

    commented rock paper scissors and started the light/dark modes

--- RockPaperScissors/script.js
+++ RockPaperScissors/script.js
@@ -1,14 +1,18 @@
+// get the elements
 const player1 = document.querySelector(".one");
 const player2 = document.querySelector(".two");
 const status = document.getElementById('status');
 
 // console.log(player1, player2, status)
+
+// make the global variables
 let one;
 let two;
 let three;
 let play1;
 let play2;
 
+// get when 1, 2, or 3 is pressed
 document.addEventListener("keydown", function(event) {
     if(event.key === "1"){
         one = true;
@@ -20,6 +24,7 @@ document.addEventListener("keydown", function(event) {
     keypress()
 });
 
+// get when 1, 2, or 3 is unpressed
 document.addEventListener("keyup", function(event) {
     if(event.key === "1"){
         one = false;
@@ -30,6 +35,7 @@ document.addEventListener("keyup", function(event) {
     }
 });
 
+// when they are pressed change the picture to the corresponding picture
 function keypress() {
     if (one){
         player1.src = 'assets/Rock.png';
@@ -41,12 +47,15 @@ function keypress() {
         player1.src = 'assets/Scissors.png';
         play1 = 'scissors';
     };
-        aipress();
+    // let the ai choose
+    aipress();
 };
 
 function aipress() {
+    // make the ai choose a random number between 1 to 3
     let randomnumber = Math.floor(Math.random()*3)+1;
     // console.log(randomnumber)
+    // change the picture to the corresponding object
     if(randomnumber == 1){
         player2.src = 'assets/Rock.png';
         play2 = 'rock';
@@ -60,23 +69,30 @@ function aipress() {
 
     // console.log(player1.src, player2.src);
     
+    // if they are the same tell them its a tie
     if(play1 == play2){
         status.innerHTML = "Its a tie!!";
     }else if(play1 == 'rock'){
+        // if player 1 chose rock and 2 chose scissors 1 wins
         if(play2 == 'scissors'){
             status.innerHTML = "Congratulations you won!!";
+            // if player 1 chose rock and 2 chose paper than 2 wins
         }else if(play2 == 'paper'){
             status.innerHTML = "Sorry you lost";
         }
     }else if(play1 == 'paper'){
+        // if player 1 chose paper and 2 chose rock 1 wins
         if(play2 == 'rock'){
             status.innerHTML = "Congratulations you won!!";
         }else if (play2 == 'scissors'){
+        // if player 1 chose paper and 2 chose scissors than 2 wins
             status.innerHTML = "Sorry you lost";
         }
     }else if(play1 == 'scissors'){
+        // if player 1 chose scissors and 2 chose paper 1 wins
         if(play2 == 'paper'){
             status.innerHTML = "Congratulations you won!!";
+        // if player 1 chose scissors and 2 chose rock 2 wins
         }else if (play2 == 'rock'){
             status.innerHTML = "Sorry you lost";
         }
