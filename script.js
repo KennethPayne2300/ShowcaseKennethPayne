0c8bd24c7d6757c53d722856217019cd732c0c32
KennethPayne2300 <kennethpayne353@gmail.com>
2025-07-01 13:08:41 -0500

    Added Headshots

--- script.js
+++ script.js
@@ -49,19 +49,10 @@ function picSwap() {
 
     function frame() {
         if (pic === 1) {
-            profile.src = "https://images.pexels.com/photos/6624305/pexels-photo-6624305.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";
+            profile.src = "Assets/Headshots/Headshot2.png";
             pic = 2;
         } else if (pic === 2) {
-            profile.src = "https://st2.depositphotos.com/2931363/6569/i/450/depositphotos_65699901-stock-photo-black-man-keeping-arms-crossed.jpg";
-            pic = 3;
-        } else if (pic === 3) {
-            profile.src = "https://image.shutterstock.com/image-photo/young-brazilian-man-isolated-on-260nw-2242569333.jpg";
-            pic = 4;
-        } else if (pic === 4) {
-            profile.src = "https://i.redd.it/j02itmog3t721.jpg";
-            pic = 5;
-        } else if (pic === 5) {
-            profile.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGh5WFH8TOIfRKxUrIgJZoDCs1yvQ4hIcppw&s";
+            profile.src = "Assets/Headshots/Headshot1.png";
             pic = 1;
         }
     }
